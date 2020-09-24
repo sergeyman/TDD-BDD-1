dcce3154db204c9a9f115cf353ef5c194cbf3086
sergeyman <sergey_man@ukr.net>
2020-09-24 08:36:07 +0300

    Adding Jest (CLI) testing for async fetch() /H

---
+++ fetch-functions.test.js
@@ -0,0 +1,17 @@
+//import {convert} from './fetch-functions';		//Jest encountered an unexpected token
+const convert = require('./fetch-functions');
+ 
+//mock fetch()  mock .function
+global.fetch = jest.fn(() =>
+	Promise.resolve({
+		json: () => Promise.resolve({ rates: { CAD: 1.42 } }),
+	})
+};
+
+it('converts USD to CAD', async () => {
+	const rate = await convert("USD", "CAD");
+	
+	expect(rate).toEqual(1.42);
+	
+	expect(fetch).toHaveBeenCalledTimes(1);
+});
--- sum.test.js
+++ sum.test.js
@@ -3,3 +3,42 @@ const sum = require('./sum');
 test('Jest/CLI: adds 1 + 2 to equal 3', () => {
 	expect(sum(1, 2)).toBe(3);
 });
+
+//Matchers (сопоставления) для тест. знечений разными способами
+test('Jest: 2 + 2 = 4', () => {
+	expect(2 + 2).toBe(4);			//exact equality
+});
+
+test('Jest: Присваивание объекту', () => {
+	const data = {один: 1};
+	data['два'] = 2;
+	expect(data).toEqual({один: 1, два: 2});	//check value of an object recursevly
+});
+
+test('Jest: Сложение положительных чисел не равно нулю', () => {
+	for(let a=1; a<10; a++) {
+		for(let b=1; b<10; b++) {
+			expect(a + b).not.toBe(0);
+		}
+	}
+});
+
+//Testing Asynchronous Code
+// Don't do this!
+/*
+test('the data is peanut butter', () => {
+  function callback(data) {
+    expect(data).toBe('peanut butter');
+  }
+
+  //fetchData(callback);
+  fetch('https://restcountries.eu/rest/v1/all');
+});
+*/
+
+
+/*
+https://jestjs.io/docs/ru/asynchronous
+
+
+*/
